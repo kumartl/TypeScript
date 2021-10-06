33800aa2ba116438cbe72b311fe7bcc5d9ee7da9
kumar <kumar@twilightsoftwares.com>
2021-10-06 19:48:25 +0530

    added seven files of ts

---
+++ secondstring.js
@@ -0,0 +1,14 @@
+var message = "hello world";
+//console.log(message)
+document.getElementById("demo").innerHTML = message;
+var x = 10;
+var y = '20';
+var z = x + y;
+//console.log(z)
+document.getElementById("demo1").innerHTML = z;
+function add(a, b) {
+    return a + b;
+}
+var h = add(5, 5);
+//console.log(h)
+document.getElementById("demo2").innerHTML = h;
